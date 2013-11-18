1d2b5ef90389b9dfeb6726e5514b2e91297692af
alex <alesssandro@ukr.net>
2013-11-18 14:22:39 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperclipAndDropboxSample::Application.config.secret_token = 'cbbf2e9d04569902f72c4df19706924c1f1ba7bb228382efb21daca6b3a5e0f183e4e71ec680f1a71a7fe9d7bb5e05f7c99ae6e97bc446ccc7525de7f08c9f8d'
